cfa3089eb64321f8ce66b6c4ca93a7f40015c43c
leechannl <leechannl@gmail.com>
2014-06-01 22:05:39 +0800

    Initial Commit

---
+++ iTahDoodle/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  iTahDoodle
+//
+//  Created by mm on 6/1/14.
+//  Copyright (c) 2014 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iTahDoodle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iTahDoodle
+//
+//  Created by mm on 6/1/14.
+//  Copyright (c) 2014 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
